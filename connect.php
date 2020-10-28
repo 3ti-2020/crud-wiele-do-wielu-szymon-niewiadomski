85274ff55d38ae0ada9fd094875654449f618321
propsu <49064061+propsu@users.noreply.github.com>
2020-10-28 08:21:37 +0100

    Add files via upload

--- connect.php
+++ connect.php
@@ -2,5 +2,8 @@
 
 $db = new mysqli('hs5.linux.pl', 'niewiado_skillmon', 'Pokemon123', 'szkola');
 
-
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
 ?>
