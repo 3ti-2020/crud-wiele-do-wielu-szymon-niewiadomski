6068e4c9529556ea00c8070770157c7e3dd07533
propsu <49064061+propsu@users.noreply.github.com>
2020-10-28 09:07:37 +0100

    Insert

--- index.php
+++ index.php
@@ -6,14 +6,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library</title>
+    <title>Szymon Niewiadomski</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <main>
-        <h1>Biblioteka</h1>
+    <header class="header">
+        <h1>Szymon Niewiadomski</h1>
+        <h3>Biblioteka</h3>
+    </header>
+    <aside class="aside">
+        <div class="insert">
+            <form action="insert.php" method="post" autocomplete="off" >
+                <input type="text" name="name" class="input" placeholder="Imię" required>
+                <input type="text" name="lastname" class="input" placeholder="Nazwisko" required>
+                <input type="text" name="title" class="input" placeholder="Tytuł" required>
+                <input type="submit" value="Dodaj książkę" class="button">
+            </form>
+        </div>
+    </aside>
+    <main class="main">
         <div class="library">
-            <?php createTable("SELECT * from autorzy, tytuly, ksiazki WHERE autorzy.id_autor = ksiazki.id_autor AND tytuly.id_tytul = ksiazki.id_tytul");?>
+            <?php createTable("SELECT imie, nazwisko, tytul from autorzy, tytuly, ksiazki WHERE autorzy.id_autor = ksiazki.id_autor AND tytuly.id_tytul = ksiazki.id_tytul");?>
         </div>
     </main>
 </body>
