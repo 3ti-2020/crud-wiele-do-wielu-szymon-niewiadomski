b475046c942250825f6db4aa74cf3e2fcf316855
propsu <49064061+propsu@users.noreply.github.com>
2020-11-03 10:53:18 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,5 @@
 <?php
-
-
+$db = new Mysqli("mysql-propsu.alwaysdata.net", "propsu", "P0kemon12#", "propsu_szkola");
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
   }
