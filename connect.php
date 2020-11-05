cf1295a8fa2cbe565df2b7923b57db34b89e4e69
propsu <49064061+propsu@users.noreply.github.com>
2020-11-05 13:40:17 +0100

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db = new mysqli('mysql-propsu.alwaysdata.net', 'propsu', 'P0kemon12#', 'propsu_szkola');
-
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
-  }
-?>
