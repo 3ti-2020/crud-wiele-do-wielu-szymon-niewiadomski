326044784104904ff6853199a8c97880734c0d5a
propsu <49064061+propsu@users.noreply.github.com>
2020-11-18 09:12:22 +0100

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -5,4 +5,11 @@ $db = new mysqli('mysql-propsu.alwaysdata.net', 'propsu', 'P0kemon12#', 'propsu_
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
   }
+
+function checkPermission($id){
+  if(!in_array($id, $_SESSION['permissions']))
+    die("NO PERMISSION TO ACCESS!");
+}
+
+session_start();
 ?>
