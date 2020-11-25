5ecedc2dbd73d81409804eb7c7ca024945996ab2
propsu <49064061+propsu@users.noreply.github.com>
2020-11-25 08:49:17 +0100

    Add files via upload

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -1,7 +1,8 @@
 class Task{
-    constructor(name){
+    constructor(name, doned = false){
         this.id = this.generateID();
         this.name = name;
+        this.doned = doned;
     }
 
     generateID(){
@@ -16,7 +17,7 @@ const todoList = {
     addButton: document.querySelector('.add-button'),
     init(){
         this.addTask('Dodać ciasteczka');
-        this.addTask('Wynieść śmieci');
+        this.addTask('Wynieść śmieci', true);
         this.addButton.addEventListener('click', ()=>{
             const name = this.taskInput.value;
             if(name.trim().length == 0) return;
@@ -25,8 +26,8 @@ const todoList = {
             this.addTask(name);
         });
     },
-    addTask(name){
-        const task = new Task(name);
+    addTask(name, doned = false){
+        const task = new Task(name, doned);
         this.tasksArray.push(task);
         this.appendTask(task);
     },
@@ -36,20 +37,27 @@ const todoList = {
         li.innerText = task.name;
         li.setAttribute('data-id', task.id);
 
+        if(task.doned) li.classList.add('doned');
+        li.addEventListener('click', ()=>{ this.switchTask(li); });
+
         const removeButton = document.createElement('button');
         removeButton.innerText = "Usuń";
         removeButton.classList.add('remove-button');
-        removeButton.addEventListener('click', ()=>{
-            const id = li.getAttribute('data-id');
-            this.removeTask(id);
-        });
+        removeButton.addEventListener('click', ()=>{ this.removeTask(li); });
+        
         li.append(removeButton);
-
         this.tasksList.append(li);
     },
-    removeTask(id){
-        document.querySelector(`li[data-id="${id}"]`).remove();
+    removeTask(li){
+        const id = li.getAttribute('data-id');
+        li.remove();
         this.tasksArray = this.tasksArray.filter(task=> task.id != id);
+    },
+    switchTask(li){
+        li.classList.toggle('doned');
+        const id = li.getAttribute('data-id');
+        const task = this.tasksArray.find(task => task.id == id);
+        task.doned = !task.doned;
     }
 }
 
